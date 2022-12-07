b983d9bb9ccb6bbed214d7b89a89779028f807da
Ryan Allred <ryanallred91@gmail.com>
2022-12-07 11:25:29 -0600

    First two bullet points finished

---
+++ IRobber.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PlanYourHeist2
+{
+    public interface IRobber
+    {
+         string Name {get; set;}
+         int SkillLevel {get; set;}
+         int PercentageCut {get; set;}
+         void PerformSkill(Bank b);
+    }
+}
